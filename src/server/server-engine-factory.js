9f88308779b259b98b3ca8ce29666e0991ea97f0
Ivan Santos <pragmaticivan@gmail.com>
2017-02-03 15:33:58 -0800

    Adds server engine factory documentation

--- src/server/server-engine-factory.js
+++ src/server/server-engine-factory.js
@@ -1,15 +1,26 @@
-import ExpressEngine from "./express-engine";
+import ExpressEngine from './express-engine';
 
+/**
+ * Server engine factory
+ */
 class ServerEngineFactory {
+  /**
+   * Constructor
+   * @param  {String} type Engine type
+   */
   constructor(type = 'express') {
     this.factoryType = type;
   }
 
+  /**
+   * Creates factory.
+   * @return {BaseEngine|Error}
+   */
   create() {
     if(this.factoryType === 'express') {
       return new ExpressEngine();
     } else {
-      throw new Error("Engine not implemented.");
+      throw new Error('Engine not implemented.');
     }
   }
 
