d38a1f45ca5261de596c19cd903a91e4323239d8
Ivan Santos <pragmaticivan@gmail.com>
2017-02-22 08:01:02 -0800

    Fixes const usage on error middleware test

--- test/unit/middleware/error.js
+++ test/unit/middleware/error.js
@@ -3,14 +3,14 @@ import httpMocks from 'node-mocks-http';
 
 describe('errorMiddleware', function() {
   it('should create an instance of a server', () => {
-    let request = httpMocks.createRequest({
+    const request = httpMocks.createRequest({
         method: 'GET',
         url: '/test/path?myid=312',
         query: {
             myid: '312',
         },
     });
-    let response = httpMocks.createResponse();
+    const response = httpMocks.createResponse();
 
     errorMiddleware()({foo: 'bar'}, request, response, (err) => {
       expect(err).to.deep.equal({foo: 'bar'});
