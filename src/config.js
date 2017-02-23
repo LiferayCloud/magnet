bba43912a42f7a9105f8825b3f82841f4efab8b6
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2017-02-23 12:02:44 -0800

    Uses default host 0.0.0.0 to bind by default to loopback ip

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ import path from 'path';
 const baseConfig = () => {
   return {
     magnet: {
-      host: 'localhost',
+      host: '0.0.0.0',
       ignore: [
           'build/**',
           'coverage/**',
