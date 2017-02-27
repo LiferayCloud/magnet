4fdff0f3f4539271c2b5d23e9e0ac07d24a57d2c
Ivan Santos <pragmaticivan@gmail.com>
2017-02-27 15:26:49 -0800

    Updates testes for string registrator

---
+++ test/fixtures/string_registrator_app/no-type.js
@@ -0,0 +1,6 @@
+export const route = {
+  path: '/string-no-type',
+  method: 'get',
+};
+
+export default 'no type defaults to html';
---
+++ test/fixtures/string_registrator_app/valid.js
@@ -0,0 +1,7 @@
+export const route = {
+  path: '/string-true',
+  method: 'get',
+  type: 'json',
+};
+
+export default 'valid string';
