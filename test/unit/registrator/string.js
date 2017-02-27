132da98082fb5846f1c2f1fd2b76c0d37ff1bfd8
Ivan Santos <pragmaticivan@gmail.com>
2017-02-27 15:26:49 -0800

    Updates testes for string registrator

--- test/unit/registrator/string.js
+++ test/unit/registrator/string.js
@@ -4,85 +4,32 @@ import registratorString from '../../../src/registrator/string';
 
 describe('registratorString', () => {
   describe('.test', () => {
-    it('should perform the test validation for the input and return true', () => { // eslint-disable-line max-len
+    it('should return true if module object have a route attribute and the default attribute is a string', () => { // eslint-disable-line max-len
       const testFn = {};
-      testFn.default = 'custom string';
+      testFn.default = 'foo';
       testFn.route = {};
       expect(registratorString.test(null, testFn, null)).to.be.true;
     });
 
-    it('should perform the test validation for the input and return false if that\'s a string', () => { // eslint-disable-line max-len
+    it('should return false if in the module there\'s a route attribute but it\'s not an object and the default attribute is a string', () => { // eslint-disable-line max-len
+      const testFn = {};
+      testFn.default = 'foo';
+      testFn.route = 'foo';
+
+      expect(registratorString.test(null, testFn, null)).to.be.false;
+    });
+
+    it('should return false if in the module the route attribute is an object and the default attribute is not a string', () => { // eslint-disable-line max-len
       const testFn = {};
       testFn.default = () => {};
       testFn.route = {};
+
       expect(registratorString.test(null, testFn, null)).to.be.false;
     });
   });
 
   describe('.register', () => {
     const directory = `${process.cwd()}/test/fixtures/app`;
-
-    it('should register a function on magnet\'s current server engine', () => {
-      const magnet = new Magnet({directory});
-      const testFn = {
-        route: {
-          path: '/fn',
-          method: 'post',
-          type: 'html',
-        },
-      };
-      testFn.default = (req, res) => res.end('fn');
-
-      registratorString.register('foo.js', testFn, magnet);
-
-      const app = magnet.getServer().getEngine();
-      const routes = app._router
-        .stack
-        .filter((stack) => {
-          if (stack.route && stack.route.path) {
-            return true;
-          }
-        })
-        .map((stack) => {
-          return stack.route.path;
-        });
-
-      expect(routes).to.include('/fn');
-    });
-
-    it('should default to get if method is not provided', () => {
-      const magnet = new Magnet({directory});
-      const testFn = {
-        route: {
-          path: '/fn',
-          type: 'html',
-        },
-      };
-      testFn.default = (req, res) => res.end('fn');
-
-      registratorString.register('foo.js', testFn, magnet);
-
-      const app = magnet.getServer().getEngine();
-
-      const routes = app._router
-        .stack
-        .filter((stack) => {
-          if (stack.route && stack.route.path) {
-            return true;
-          }
-        })
-        .map((stack) => {
-          return {
-            path: stack.route.path,
-            methods: stack.route.methods,
-          };
-        });
-
-      expect(routes[0].path).to.equal('/fn');
-      expect(routes[0].methods).to.include({get: true});
-    });
-
-    it('should set default to html if type is not provided');
 
     it('should throw an error if provided method is not a string', () => {
       const magnet = new Magnet({directory});
@@ -104,14 +51,31 @@ describe('registratorString', () => {
       }).to.throw(Error, 'Route configuration method must be a string, check /foo.js.'); // eslint-disable-line max-len
     });
 
-    it('should throw an error if route configuration path is not provided ', () => { // eslint-disable-line max-len
+    it('should throw an error if route configuration path is null', () => {
+      const magnet = new Magnet({directory});
+      const testFn = {
+        route: {
+          path: null,
+        },
+      };
+
+      testFn.default = (req, res) => res.end('fn');
+      expect(function() {
+        registratorString.register(
+          path.join(magnet.getServerDistDirectory(), 'foo.js'),
+          testFn,
+          magnet,
+        );
+      }).to.throw(Error, 'Route configuration path must be specified, check /foo.js.'); // eslint-disable-line max-len
+    });
+
+    it('should throw an error if route configuration path is undefined ', () => { // eslint-disable-line max-len
       const magnet = new Magnet({directory});
       const testFn = {
         route: {
         },
       };
       testFn.default = (req, res) => res.end('fn');
-
       expect(function() {
         registratorString.register(
           path.join(magnet.getServerDistDirectory(), 'foo.js'),
@@ -140,6 +104,40 @@ describe('registratorString', () => {
         port: 3000,
         path: '/string',
         responseBody: htmlString,
+      });
+
+      await magnet.stop();
+    });
+  });
+
+  describe('Integration with application folder and http server', () => {
+    it('should register a module from the directory when its default attribute is a string and it has a route object attribute', async() => { // eslint-disable-line max-len
+      const directory = `${process.cwd()}/test/fixtures/string_registrator_app`; // eslint-disable-line max-len
+      const magnet = new Magnet({directory});
+
+      await magnet.build();
+      await magnet.start();
+
+      await assertAsyncHttpRequest({
+        port: 3000,
+        path: '/string-true',
+        responseBody: 'valid string',
+      });
+
+      await magnet.stop();
+    });
+
+    it('should set default to html if type is not provided', async () => {
+      const directory = `${process.cwd()}/test/fixtures/string_registrator_app`; // eslint-disable-line max-len
+      const magnet = new Magnet({directory});
+
+      await magnet.build();
+      await magnet.start();
+
+      await assertAsyncHttpRequest({
+        port: 3000,
+        path: '/string-no-type',
+        contentType: 'text/html; charset=utf-8',
       });
 
       await magnet.stop();
