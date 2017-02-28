fb00252639b6b07ea12db2c466cd33f3d11bb20d
Ivan Santos <pragmaticivan@gmail.com>
2017-02-28 10:17:06 -0800

    Adds tests for validator error middleware

---
+++ test/fixtures/error/error-validation.js
@@ -0,0 +1,14 @@
+export const route = {
+  path: '/error-validation',
+  method: 'get',
+  type: 'json',
+};
+
+export default async (req) => {
+  req.assert('name', 'parameter_required').notEmpty();
+
+  let validation = await req.getValidationResult();
+  validation.throw();
+
+  return {hello: req.query.name};
+};
