5e9c272056b154ba158831d304470389618372e2
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2017-02-27 19:08:56 -0800

    Source formatting tests

--- test/unit/middleware/error.js
+++ test/unit/middleware/error.js
@@ -2,19 +2,15 @@ import Magnet from '../../../src/magnet';
 
 describe('errorMiddleware', function() {
   it('should manage to catch an exception if an error occurs in a route function', async () => { // eslint-disable-line max-len
-    const directory = `${process.cwd()}/test/fixtures/error_simulation_app`; // eslint-disable-line max-len
+    const directory = `${process.cwd()}/test/fixtures/error`;
       const magnet = new Magnet({directory});
-
       await magnet.build();
       await magnet.start();
-
       await assertAsyncHttpRequest({
-        port: 3000,
         path: '/fn-error-environment',
         status: 500,
         responseBody: JSON.stringify({status: 500, message: 'error message'}),
       });
-
       await magnet.stop();
   });
 });
