4a47356608b6b9b58deef8f7dafe508bbf562cce
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2017-02-27 16:20:14 -0800

    Recognizes “errors” key on Error objects

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -6,10 +6,13 @@ export function errorMiddleware() {
   return (err, req, res, next) => {
     const status = err.status || 500;
     const message = err.message || '';
-    res.status(status);
-    res.json({
+    let result = {
       status,
       message,
-    });
+    };
+    if (err.errors) {
+      result.errors = err.errors;
+    }
+    res.status(status).json(result);
   };
 }
