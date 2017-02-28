5fe4bc53c5d609697d97398464a9669e338a0ea2
Ivan Santos <pragmaticivan@gmail.com>
2017-02-28 13:15:40 -0800

    Improves coverage for .build()

--- test/unit/build/build.js
+++ test/unit/build/build.js
@@ -1,32 +1,57 @@
 import {build} from '../../../src/build/build';
-import {existsSync} from 'fs';
+import {existsSync, mkdirSync, writeFileSync} from 'fs';
 import del from 'del';
 import Magnet from '../../../src/magnet';
 import path from 'path';
 
-describe('.build', function() {
+describe.only('.build', function() {
   const directory = `${process.cwd()}/test/fixtures/build`;
+  let magnet = null;
+  let serverDist = null;
+
+  beforeEach(() => {
+    magnet = new Magnet({directory});
+    serverDist = path.join(directory, '.magnet');
+  });
+
+  afterEach(async () => {
+    await del(serverDist);
+  });
 
   it('should build the specified app directory', async() => {
-    const magnet = new Magnet({directory});
-    const serverDist = path.join(directory, '.magnet');
     await build(magnet.getFiles(directory), directory, serverDist);
+
     expect(existsSync(serverDist)).to.be.true;
-    await del(serverDist);
   });
 
   it('should build the files inside the specified app directory', async() => {
-    const magnet = new Magnet({directory});
-    const serverDist = path.join(directory, '.magnet');
     await build(magnet.getFiles(directory), directory, serverDist);
+
     expect(existsSync(path.join(serverDist, 'one.js'))).to.be.true;
     expect(existsSync(path.join(serverDist, 'two.js'))).to.be.true;
-    await del(serverDist);
   });
 
-  it('should clean dist directory before build');
+  it('should clean dist directory before build', async() => {
+    if(!existsSync(serverDist)) {
+      mkdirSync(serverDist);
+    }
 
-  it('should inform the user that the app doesn\'t have javascript files to register'); // eslint-disable-line max-len
+    const mockedFile = path.join(serverDist, 'testfile');
+    if(!existsSync(mockedFile)) {
+      writeFileSync(mockedFile, 'testfile content');
+    }
 
-  it('should not build anything from assets folder, and instead just copy the file'); // eslint-disable-line max-len
+    expect(existsSync(mockedFile)).to.be.true;
+
+    await build(magnet.getFiles(directory), directory, serverDist);
+
+    expect(existsSync(mockedFile)).to.be.false;
+  });
+
+  it('should inform the user that the app doesn\'t have javascript files to register', (done) => { // eslint-disable-line max-len
+    build([], directory, serverDist).catch((error) => {
+      expect(error.name).to.equal('WebpackOptionsValidationError');
+      done();
+    });
+  });
 });
