1fb6ffa6ad9cf22ed329c1b6492a30268cece7d3
NetoDevelm <josevieira.dev@gmail.com>
2017-03-13 22:20:28 -0300

    Updates public methods get/set port and hostname

--- test/unit/server-factory.js
+++ test/unit/server-factory.js
@@ -3,8 +3,19 @@ import ServerFactory from '../../src/server-factory';
 
 describe('ServerFactory', function() {
   it('should create an instance of a server', () => {
-    let server = ServerFactory.create(ServerFactory.EXPRESS, 3000, "0.0.0.0");
+    let server = ServerFactory.create();
     expect(server instanceof Server).to.equal(true);
+  });
+
+  it('should create an instance of a server with express by default', () => {
+    let server = ServerFactory.create();
+    expect(isExpress(server.getEngine())).to.equal(true);
+  });
+
+  it('should create an instance of a express by passing a param', () => {
+    const server = ServerFactory.create(
+      ServerFactory.Types.EXPRESS);
+      expect(isExpress(server.getEngine())).to.equal(true);
   });
 
   it('should thrown an error if the specified type is not implemented', () => {
@@ -13,16 +24,5 @@ describe('ServerFactory', function() {
     }).to.throw('Engine not implemented');
   });
 
-  it('should thrown an error if the specified port is not implemented', () => {
-    expect(function() {
-      ServerFactory.create(ServerFactory.EXPRESS, null, "0.0.0.0");
-    }).to.throw('port server is required');
-  });
-
-  it('should thrown an error if the specified host is not implemented', () => {
-    expect(function() {
-      ServerFactory.create(ServerFactory.EXPRESS, 3000, null);
-    }).to.throw('host server is required');
-  });
-
+  
 });
