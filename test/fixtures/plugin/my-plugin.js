decc3f26ac10a59e634a7b9b482c5645c7d028db
Michael Hadley <michael.hadley@liferay.com>
2017-05-15 18:27:51 -0700

    Adds test for plugin method ordering (#88)

--- test/fixtures/plugin/my-plugin.js
+++ test/fixtures/plugin/my-plugin.js
@@ -1,5 +1,14 @@
 export default {
   start(magnet) {
     magnet.__START_WAS_CALLED__ = true;
+    magnet.__LAST_PLUGIN_METHOD__ = 'start';
+  },
+
+  register(module, file, magnet) {
+    magnet.__LAST_PLUGIN_METHOD__ = 'register';
+  },
+
+  test() {
+    return true;
   },
 };
