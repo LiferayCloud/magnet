6dbad08f646c48b224fe1b762d2cc6c82a4984d1
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2017-05-31 11:23:48 -0700

    Build

--- examples/metal-server-side-rendering/page1.soy.js
+++ examples/metal-server-side-rendering/page1.soy.js
@@ -1,63 +1,46 @@
 /* jshint ignore:start */
 import Component from 'metal-component';
 import Soy from 'metal-soy';
+
 var templates;
 goog.loadModule(function(exports) {
-
+var soy = goog.require('soy');
+var soydata = goog.require('soydata');
 // This file was automatically generated from page1.soy.
 // Please don't edit this file by hand.
 
 /**
  * @fileoverview Templates in namespace Page1.
- * @suppress {missingRequire}
  * @public
  */
 
 goog.module('Page1.incrementaldom');
 
-/** @suppress {extraRequire} */
-goog.require('goog.asserts');
-/** @suppress {extraRequire} */
-goog.require('goog.i18n.bidi');
-/** @suppress {extraRequire} */
-goog.require('goog.string');
-/** @suppress {extraRequire} */
-var soy = goog.require('soy');
-/** @suppress {extraRequire} */
-var soydata = goog.require('soydata');
-var IncrementalDom = goog.require('incrementaldom');
-var ie_open = IncrementalDom.elementOpen;
-var ie_close = IncrementalDom.elementClose;
-var ie_void = IncrementalDom.elementVoid;
-var ie_open_start = IncrementalDom.elementOpenStart;
-var ie_open_end = IncrementalDom.elementOpenEnd;
-var itext = IncrementalDom.text;
-var iattr = IncrementalDom.attr;
+var incrementalDom = goog.require('incrementaldom');
 var soyIdom = goog.require('soy.idom');
-var dyn = soyIdom.renderDynamicContent;
-var print = soyIdom.print;
 
 
 /**
  * @param {Object<string, *>=} opt_data
- * @param {(null|undefined)=} opt_ignored
  * @param {Object<string, *>=} opt_ijData
+ * @param {Object<string, *>=} opt_ijData_deprecated
  * @return {void}
  * @suppress {checkTypes}
  */
-function $render(opt_data, opt_ignored, opt_ijData) {
-  ie_open('div');
-    ie_open('h1');
-      itext('Hello ');
-      print(opt_data.attr);
-      itext('!');
-    ie_close('h1');
-    ie_open_start('a');
-        iattr('href', '/page2');
-    ie_open_end();
-      itext('Navigate');
-    ie_close('a');
-  ie_close('div');
+function $render(opt_data, opt_ijData, opt_ijData_deprecated) {
+  opt_ijData = opt_ijData_deprecated || opt_ijData;
+  incrementalDom.elementOpen('div');
+    incrementalDom.elementOpen('h1');
+      incrementalDom.text('Hello ');
+      soyIdom.print(opt_data.attr);
+      incrementalDom.text('!');
+    incrementalDom.elementClose('h1');
+    incrementalDom.elementOpenStart('a');
+        incrementalDom.attr('href', '/page2');
+    incrementalDom.elementOpenEnd();
+      incrementalDom.text('Navigate');
+    incrementalDom.elementClose('a');
+  incrementalDom.elementClose('div');
 }
 exports.render = $render;
 if (goog.DEBUG) {
