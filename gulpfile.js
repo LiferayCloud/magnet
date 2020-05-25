f1387ad3aa8970e62a6245c4d9ca5d55871f09d3
Ygor Costa <ygor.costa@liferay.com>
2020-05-25 10:20:18 -0300

    Update webpack version

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
+const {src, dest, series} = require('gulp');
 const babel = require('gulp-babel');
-const gulp = require('gulp');
 const del = require('del');
 
-gulp.task('build', ['clean'], () =>
-  gulp
-    .src(['./src/**/*', '!./src/**/*.test.js'])
-    .pipe(babel())
-    .pipe(gulp.dest('build'))
-);
 
-gulp.task('clean', () => del('build'));
+/**
+ * @return {Function}
+ */
+function js() {
+  return src(['./src/**/*', '!./src/**/*.test.js'])
+  .pipe(babel())
+  .pipe(dest('build'));
+}
+
+/**
+ * @return {Promise}
+ */
+function deleteBuildFolder() {
+  return del('build');
+}
+
+exports.deleteBuildFolder = deleteBuildFolder;
+exports.js = js;
+exports.default = series(deleteBuildFolder, js);
